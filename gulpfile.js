765ca57554d4dfbca59b643a235d5211767d245d
Jerfferson Ferreira <jerffersonfs@gmail.com>
2016-12-21 23:14:15 -0200

    Adição das tasks de javascript

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+/* globals require */
+/* jshint esversion:6 */
+
+(function () {
+	'use strict';
+
+	const
+		JS_SOURCE_PATH = './source/js',
+		JS_DIST_PATH = './dist/js';
+
+	const gulp = require('gulp'),
+		browserify = require('browserify'),
+		source = require('vinyl-source-stream'),
+		buffer = require('vinyl-buffer'),
+		uglify = require('gulp-uglify'),
+		sourcemaps = require('gulp-sourcemaps'),
+		gutil = require('gulp-util'),
+		clean = require('gulp-clean'),
+		jshint = require('gulp-jshint');
+
+	gulp.task('js:clean', function () {
+		return gulp.src(JS_DIST_PATH, {read: false})
+			.pipe(clean({force: true}));
+	});
+
+	gulp.task('js:jshint', function() {
+		return gulp.src(JS_SOURCE_PATH + '/*.js')
+			.pipe(jshint())
+			.pipe(jshint.reporter('default'))
+			.pipe(jshint.reporter('jshint-stylish'))
+			.pipe(jshint.reporter('fail'));
+	});
+
+	gulp.task('js:browserify', ['js:jshint', 'js:clean'], function () {
+		var b = browserify({
+			debug: true,
+			basedir: JS_SOURCE_PATH + '/',
+			paths: [JS_SOURCE_PATH + '/']
+		});
+
+		return b.bundle()
+			.pipe(source('main.js'))
+			.pipe(buffer())
+			.pipe(sourcemaps.init({loadMaps: true}))
+			.pipe(uglify())
+			.on('error', gutil.log)
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest(JS_DIST_PATH + '/'));
+	});
+
+	gulp.task('watch', function () {
+		gulp.watch(JS_SOURCE_PATH + '/*.js', ['js:browserify']);
+	});
+})();
